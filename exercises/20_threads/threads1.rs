f857a38d2c6a2a75f297a9c555a5ad1f329bf70d
abhay-psr <a6hay.c.psr@gmail.com>
2025-04-14 01:32:23 +0530

    20.1

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -24,6 +24,9 @@ fn main() {
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        if let Ok(c) = handle.join() {
+            results.push(c);
+        }
     }
 
     if results.len() != 10 {
