df39390c4e2b14083fce5d660967b5ec9bcbe8ad
abhay-psr <a6hay.c.psr@gmail.com>
2025-04-13 15:56:21 +0530

    14.1

---
+++ scripts/wf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: ./<path>/wf.sh command filepath"
+    echo ""
+    echo "Command: diff, copy"
+    echo ""
+fi
+
+
+command=$1
+filepath=$2
+
+function show_diff {
+    echo "Filepath: $1"
+    filea=$1
+    fileb=`echo $1 | sed s/exercises/solutions/`
+    diff --color -y ../rustlings/{$filea,$fileb}
+}
+
+function copy_file {
+    echo "Filepath: $1"
+    cp ../rustlings/$1 $1
+}
+
+
+case $command in 
+    ( "diff" ) show_diff $filepath;;
+    ( "copy" ) copy_file $filepath;;
+esac
